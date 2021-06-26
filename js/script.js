a9e821f5c89cbf57a809f71792d480b4779f5875
Killershark77 <franciscotrianto@gmail.com>
2021-06-26 20:36:38 +0700

    Scroll Down Animation
    Menambah animasi indikator scroll down

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ var interval = setInterval(function(){
     timesrun += 1;
     if(timesrun === 100 || document.getElementsByClassName("loader-wrapper").display === undefined){
         clearInterval(interval);
-        gsap.from('.navbar',{ duration: 4,y : '-100%',ease: 'elastic',delay : 0})
-        gsap.from('.jumbotron',{opacity:0,duration:2,y:'-8%',x:'-50',})
+        gsap.from('.navbar',{ duration: 2,y : '-100%',ease: 'bounce'})
+        gsap.from('.jumbotron',{opacity:0,duration:2,y:'-8%',x:'-50'})
+        gsap.fromTo('.scroll-wrapper',{opacity:0,y:'200%',duration:1},{opacity:100,y:'0'})
+        gsap.to('.arrow',{opacity:'100%',y:'30',delay:1,duration:1,repeat:'-1',yoyo:true})
     }
 }, 150);
 
