b443288f2ce52c6ce550531b356885e65472ed79
Killershark77 <franciscotrianto@gmail.com>
2021-06-29 13:23:25 +0700

    Overwrite
    ???

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ var interval = setInterval(function () {
     clearInterval(interval);
     gsap.from(".navbar", { duration: 2, y: "-100%", ease: "bounce" });
     //gsap.from(".jumbotron", { opacity: 0, duration: 2, y: "-8%", x: "-50" });
+
+    
+    //RAINBOW COLOR CYCLE
     var rainbow = gsap.timeline({yoyo:true,repeat:-1,duration:1.5});
     
     rainbow.to("html",1,{"--fontColor":"rgba(175, 49, 49, 0.8)"})
@@ -16,13 +19,17 @@ var interval = setInterval(function () {
 
     rainbow.repeat();
 
+    // Jumbotron Text Animation
     var coolAnim = gsap.timeline({});
     coolAnim.fromTo('.jumbotron-texta',{scale:'0',opacity:'0'},{scale:'2',opacity:'100',duration:1.5,y:'20%'},'<');
     coolAnim.to('.jumbotron-texta',{scale:'1',opacity:'100',duration:1.5});
     coolAnim.fromTo('.jumbotron-textb',{scale:0,y:'5%'},{scale:1,opacity:100,duration:4},'<');
 
-
+    // Scroll Down Indicator
     gsap.fromTo(".scroll-wrapper",{ opacity: 0, y: "200%", duration: 2 },{ opacity: 100, y: "0",delay:4.5 });
     gsap.to(".arrow", {opacity: "100%",y: "30",delay: 1,duration: 1,repeat: "-1",yoyo: true,});
+  
+    gsap.from('.profile-container',{scrollTriger:'.scroll-trigger',opacity:20,duration:2,x:'-100%',delay:2});
+
   }
 }, 150);
