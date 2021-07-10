fa82f9597adedbcb7414fcb2816faae1235ec79a
Killershark77 <franciscotrianto@gmail.com>
2021-07-10 19:34:49 +0700

    Bagian Kegiatan Animation
    Animasi Text keluar dari garis

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+
+
+// Gsap 
+gsap.registerPlugin(ScrollTrigger);
+var A = gsap.timeline({scrollTrigger:{
+    trigger:".kegiatan_bersama_text",
+    toggleActions:"restart none restart none",
+    start:"-30px bottom",
+    //markers:true
+}});
+A.from(".kegiatan_bersama_text",{opacity:0,duration:1.5})
+A.from(".kegiatan_bersama_text",{x:-300,duration:1.7,ease: Back.easeOut.config(2.1)},"<");
+
+
+
+var B = gsap.timeline({scrollTrigger:{
+    trigger:".kegiatan_project_text",
+    toggleActions:"restart none restart restart",
+    //markers:true
+}});
+B.from(".kegiatan_project_text",{opacity:0,duration:1.5});
+B.from(".kegiatan_project_text",{x:200,duration:1.7,ease:Back.easeOut.config(2.1)},'<');
